ec71daa00c17761e165b68d467e165cc8e53bbe9
anan al amin <ananpero123@gmail.com>
2023-08-08 15:04:22 +0700

    mentahan angular

---
+++ src/app/portofolio/portofolio.component.ts
@@ -0,0 +1,14 @@
+import { Component,OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portofolio',
+  templateUrl: './portofolio.component.html',
+  styleUrls: ['./portofolio.component.css']
+})
+export class PortofolioComponent{
+}
+
+
+
+
+
---
+++ src/app/read-portofolio/read-portofolio.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-read-portofolio',
+  templateUrl: './read-portofolio.component.html',
+  styleUrls: ['./read-portofolio.component.css']
+})
+export class ReadPortofolioComponent{
+  
+}
