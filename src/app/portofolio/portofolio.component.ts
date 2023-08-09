695c675c80c395a413e2076bbd8577bfe4b25995
ananpero123 <109833759+ananpero123@users.noreply.github.com>
2023-08-09 16:59:55 +0700

    fix

--- src/app/portofolio/portofolio.component.ts
+++ src/app/portofolio/portofolio.component.ts
@@ -1,11 +1,35 @@
 import { Component,OnInit } from '@angular/core';
+import { AuthService } from 'src/app/Services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Porto } from '../Model/Porto';
+import { ApinampilService } from '../Services/apinampil.service';
+import { Firestore, collection, addDoc,collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-portofolio',
   templateUrl: './portofolio.component.html',
   styleUrls: ['./portofolio.component.css']
 })
-export class PortofolioComponent{
+export class PortofolioComponent implements OnInit {
+  userData!: Observable<any>;
+
+  constructor(private firestore:Firestore){
+    this.getData();
+  }
+
+  getData(){
+    const colectionInstance = collection(this.firestore, 'portofolio')
+    collectionData(colectionInstance, {idField: 'id'}).subscribe(val => {
+      console.log(val);
+    })
+
+    this.userData = collectionData(colectionInstance, {idField: 'id'});
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
 
 
